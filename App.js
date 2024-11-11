4c78e64003e31a71319e945b169bd4dc6a852eaf
dhlananhh <21087481.anh@student.iuh.edu.vn>
2024-11-11 14:54:08 +0700

    upload file code

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import Screen01 from './components/Screen01';
+import Screen02 from './components/Screen02';
+import Screen03 from './components/Screen03';
+
+const Stack = createNativeStackNavigator();
+
+const App = ()=>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown:false }}>
+        <Stack.Screen name="Screen01" component={ Screen01 } />
+        <Stack.Screen name="Screen02" component={ Screen02 } />
+        <Stack.Screen name="Screen03" component={ Screen03 } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
